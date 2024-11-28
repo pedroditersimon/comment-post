b7110351e6ede3ad0523e5c11fe32301294156f6
pedroditersimon <114523969+pedroditersimon@users.noreply.github.com>
2024-11-28 16:28:59 -0300

    implemented commit service

--- CommentPost.Application/Services/CommentService.cs
+++ CommentPost.Application/Services/CommentService.cs
@@ -1,19 +1,40 @@
 ﻿using CommentPost.Application.Filters;
+using CommentPost.Application.Repositories;
 using CommentPost.Domain.Entities;
 
 namespace CommentPost.Application.Services;
 
 public class CommentService
 {
+	readonly ICommentRepository _commentRepository;
+
+	public CommentService(ICommentRepository commentRepository)
+	{
+		_commentRepository = commentRepository;
+	}
+
 	// Postear comentarios: Un usuario registrado crea un nuevo comentario en la plataforma.
-	public Comment? PostNewComment(Comment comment) { return null; }
+	public Comment? PostNewComment(Comment comment)
+	{
+		return _commentRepository.Create(comment);
+	}
 
 	// Responder comentarios: Un usuario registrado responde a un comentario existente.
-	public Comment? PostNewReplyComment(Comment comment, int replyId) { return null; }
+	public Comment? PostNewReplyComment(Comment comment, int replyId)
+	{
+		comment.ReplyId = replyId;
+		return _commentRepository.Create(comment);
+	}
 
 	// Leer comentarios: Visualizar los comentarios publicados en una página.
-	public PaginationResult<Comment> GetCommentsByPageId(string pageId, Pagination pagination) { return null; }
+	public PaginationResult<Comment> GetCommentsByPageId(string pageId, Pagination pagination)
+	{
+		return _commentRepository.GetAllByPageId(pageId, pagination);
+	}
 
 	// Leer respuestas de comentarios: Un usuario lee las respuestas de un comentario.
-	public PaginationResult<Comment> GetRepliesByCommentId(int commentId, Pagination pagination) { return null; }
+	public PaginationResult<Comment> GetRepliesByCommentId(int commentId, Pagination pagination)
+	{
+		return _commentRepository.GetAllByReplyId(commentId, pagination);
+	}
 }
