73d152aa228f97cc6fed4b5bca00102f91aa377a
pedroditersimon <114523969+pedroditersimon@users.noreply.github.com>
2024-11-29 10:55:45 -0300

    configure services

---
+++ CommentPost.Infrastructure/EntityFramework/Extensions/InfraestructureServiceExtensions.cs
@@ -0,0 +1,18 @@
+﻿using CommentPost.Application.Repositories;
+using CommentPost.Application.Services;
+using CommentPost.Infrastructure.EntityFramework.Repositories;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CommentPost.Infrastructure.EntityFramework.Extensions;
+
+public static class InfraestructureServiceExtensions
+{
+	public static IServiceCollection AddInfrastructureServices(this IServiceCollection services)
+	{
+
+		services.AddScoped<ICommentRepository, CommentRepository>();
+		services.AddScoped<IUnitOfWork, UnitOfWork>();
+
+		return services;
+	}
+}
