bb6b8c2204202da35e2969fa878e06e0fc248583
pedroditersimon <114523969+pedroditersimon@users.noreply.github.com>
2024-11-29 10:55:03 -0300

    unit of work

---
+++ CommentPost.Infrastructure/EntityFramework/UnitOfWork.cs
@@ -0,0 +1,23 @@
+﻿using CommentPost.Application.Services;
+
+namespace CommentPost.Infrastructure.EntityFramework;
+
+public class UnitOfWork : IUnitOfWork
+{
+	readonly ApplicationDBContext _dbContext;
+
+	public UnitOfWork(ApplicationDBContext dbContext)
+	{
+		_dbContext = dbContext;
+	}
+
+	public void Dispose()
+	{
+		_dbContext.Dispose();
+	}
+
+	public async Task<bool> SaveAsync()
+	{
+		return await _dbContext.SaveChangesAsync() > 0;
+	}
+}
