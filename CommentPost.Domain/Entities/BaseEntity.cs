ce1654087d2b55b8a70424a92f7f73ce2b310f43
pedroditersimon <114523969+pedroditersimon@users.noreply.github.com>
2024-11-29 10:24:59 -0300

    naming

--- CommentPost.Domain/Entities/BaseEntity.cs
+++ CommentPost.Domain/Entities/BaseEntity.cs
@@ -5,7 +5,7 @@ public class BaseEntity<Tid> : ICloneable
 	public Tid ID { get; set; }
 
 	public DateTime CreationDate { get; set; }
-	public DateTime LastUpdateDate { get; set; }
+	public DateTime LastUpdatedDate { get; set; }
 	public bool IsDeleted { get; set; }
 
 	public object Clone() => this.MemberwiseClone();
