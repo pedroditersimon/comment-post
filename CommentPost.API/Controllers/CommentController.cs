85bd6c0c289074faf5f2fe82fb2360b279891dc0
pedroditersimon <114523969+pedroditersimon@users.noreply.github.com>
2024-12-02 19:06:49 -0300

    comment controller

---
+++ CommentPost.API/Controllers/CommentController.cs
@@ -0,0 +1,16 @@
+﻿using CommentPost.Domain.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CommentPost.API.Controllers;
+
+[ApiController]
+[Route("comments")]
+public class CommentController
+{
+
+	[HttpGet("{id}")]
+	public ActionResult<Comment> Get(int id)
+	{
+		return new Comment() { ID = id, Text = "comment text" };
+	}
+}
