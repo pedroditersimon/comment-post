f6dcb5a72be3ca0fc492d6336b26fbc6af181c1f
pedroditersimon <114523969+pedroditersimon@users.noreply.github.com>
2024-12-04 16:20:32 -0300

    reorganize directory

---
+++ CommentPost.Application/UseCases/Comments/GetRepliesByCommentId.cs
@@ -0,0 +1,29 @@
+﻿namespace CommentPost.Application.UseCases.Comments;
+
+using CommentPost.Application.Filters;
+using CommentPost.Application.Repositories;
+using CommentPost.Domain.Entities;
+
+// Leer respuestas de comentarios: Un usuario lee las respuestas de un comentario.
+public class GetRepliesByCommentIdCommand : Pagination
+{
+    public int CommentId { get; set; }
+}
+
+public class GetRepliesByCommentIdHandler
+{
+    readonly ICommentRepository _commentRepository;
+
+    public GetRepliesByCommentIdHandler(ICommentRepository commentRepository)
+    {
+        _commentRepository = commentRepository;
+    }
+
+
+    public async Task<PaginationResult<Comment>> ExecuteAsync(GetRepliesByCommentIdCommand command)
+    {
+        Pagination pagination = command;
+
+        return await _commentRepository.GetAllByReplyId(command.CommentId, pagination);
+    }
+}
---
+++ CommentPost.Application/UseCases/Comments/PostNewReplyComment.cs
@@ -0,0 +1,59 @@
+﻿using CommentPost.Application.Repositories;
+using CommentPost.Application.Services;
+using CommentPost.Domain.Entities;
+
+namespace CommentPost.Application.UseCases.Comments;
+
+// Responder comentarios: Un usuario registrado responde a un comentario existente.
+
+public class PostNewReplyCommentCommand
+{
+    public int UserId { get; set; }
+    public string Text { get; set; }
+    public int ReplyId { get; set; }
+}
+
+public class PostNewReplyCommentHandler
+{
+    readonly ICommentRepository _commentRepository;
+    readonly IUnitOfWork _unitOfWork;
+
+    public PostNewReplyCommentHandler(IUnitOfWork unitOfWork)
+    {
+        _commentRepository = unitOfWork.CommentRepository;
+        _unitOfWork = unitOfWork;
+    }
+
+    public async Task<Comment?> ExecuteAsync(PostNewReplyCommentCommand command)
+    {
+        Comment? originalComment = await _commentRepository.GetById(command.ReplyId);
+
+        // not found
+        if (originalComment == null)
+            return null;
+
+        // map
+        Comment comment = new()
+        {
+            UserId = command.UserId,
+
+            // if original comment is already a reply, point to the same reply
+            ReplyId = originalComment.ReplyId ?? originalComment.ID,
+
+            // inherit page id from original comment
+            PageId = originalComment.PageId,
+
+            Text = command.Text,
+        };
+
+
+        // create
+        Comment? createdComment = await _commentRepository.Create(comment);
+
+        // save
+        bool saved = await _unitOfWork.ApplyChangesAsync();
+        if (!saved) return null;
+
+        return createdComment;
+    }
+}
