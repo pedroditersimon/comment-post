50709a4418c959efb561ad387ec15c0e9e1707b5
pedroditersimon <114523969+pedroditersimon@users.noreply.github.com>
2024-12-04 16:01:43 -0300

    constructor

--- CommentPost.Application/Services/CommentService.cs
+++ CommentPost.Application/Services/CommentService.cs
@@ -1,6 +1,16 @@
-﻿namespace CommentPost.Application.Services;
+﻿using CommentPost.Application.Repositories;
+
+namespace CommentPost.Application.Services;
 
 public class CommentService
 {
 
+	readonly IUnitOfWork _unityOfWork;
+	readonly ICommentRepository _commentRepository;
+
+	public CommentService(IUnitOfWork unitOfWork)
+	{
+		_commentRepository = unitOfWork.CommentRepository;
+		_unityOfWork = unitOfWork;
+	}
 }
