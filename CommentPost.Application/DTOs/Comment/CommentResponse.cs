f00fd0c67d30dcaf95845e07b7d4b363eb1987e9
pedroditersimon <114523969+pedroditersimon@users.noreply.github.com>
2024-12-04 15:10:33 -0300

    DTOs and mappers

---
+++ CommentPost.Application/DTOs/Comment/CommentResponse.cs
@@ -0,0 +1,24 @@
+﻿namespace CommentPost.Application.DTOs.Comment;
+
+public class CommentResponse : ICloneable
+{
+	public int ID { get; set; }
+
+	public DateTime CreationDate { get; set; }
+	public DateTime LastUpdatedDate { get; set; }
+
+
+	public int UserId { get; set; }
+
+	public string PageId { get; set; }
+
+	public string Text { get; set; }
+	public bool Visibility { get; set; } = true;
+
+	public int? ReplyId { get; set; }
+
+	public object Clone()
+	{
+		return this.MemberwiseClone();
+	}
+}
---
+++ CommentPost.Application/Mappers/MappingExtensions_Comment.cs
@@ -0,0 +1,14 @@
+﻿using CommentPost.Application.DTOs.Comment;
+using CommentPost.Domain.Entities;
+
+namespace CommentPost.Application.Mappers;
+
+public static partial class MappingExtensions
+{
+
+	public static CommentResponse ToResponse(this Comment comment)
+	{
+		return new CommentResponse().ReplaceWith(comment);
+	}
+
+}
