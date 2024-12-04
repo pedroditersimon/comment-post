1978c3666eb13f6664f9b9e28c202515fdc6f8bb
pedroditersimon <114523969+pedroditersimon@users.noreply.github.com>
2024-12-04 16:45:30 -0300

    null comprobation

--- CommentPost.Application/Mappers/MappingExtensions_ReplaceWith.cs
+++ CommentPost.Application/Mappers/MappingExtensions_ReplaceWith.cs
@@ -3,9 +3,12 @@
 public static partial class MappingExtensions
 {
 	// Generic method that replaces values
-	public static TModel ReplaceWith<TModel, TReplace>(this TModel original, TReplace replaceModel)
+	public static TModel ReplaceWith<TModel, TReplace>(this TModel original, TReplace replaceModel, bool skipNullValues = false)
 		where TModel : ICloneable
 	{
+		if (original == null || replaceModel == null)
+			throw new NullReferenceException();
+
 		TModel originalClone = (TModel)original.Clone();
 
 		// Gets the properties of both models
@@ -22,6 +25,10 @@ public static partial class MappingExtensions
 			{
 				// Gets the replacement value
 				var replaceValue = replaceProp.GetValue(replaceModel);
+
+				// skip null replace values
+				if (skipNullValues && replaceValue == null)
+					continue;
 
 				originalProp.SetValue(originalClone, replaceValue);
 			}
