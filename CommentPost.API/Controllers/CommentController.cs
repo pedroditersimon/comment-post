52b5d259b85bf0c569c6c9ce42dd1da2c10be271
pedroditersimon <114523969+pedroditersimon@users.noreply.github.com>
2024-12-04 16:03:20 -0300

    post comment and reply

--- CommentPost.API/Controllers/CommentController.cs
+++ CommentPost.API/Controllers/CommentController.cs
@@ -1,6 +1,8 @@
 ﻿using CommentPost.Application.DTOs.Comment;
 using CommentPost.Application.Mappers;
 using CommentPost.Application.Repositories;
+using CommentPost.Application.Services;
+using CommentPost.Application.UseCases;
 using CommentPost.Domain.Entities;
 using Microsoft.AspNetCore.Mvc;
 
@@ -10,11 +12,13 @@ namespace CommentPost.API.Controllers;
 [Route("comments")]
 public class CommentController : ControllerBase
 {
+	readonly IUnitOfWork _unityOfWork;
 	readonly ICommentRepository _commentRepository;
 
-	public CommentController(ICommentRepository commentRepository)
+	public CommentController(IUnitOfWork unitOfWork)
 	{
-		_commentRepository = commentRepository;
+		_commentRepository = unitOfWork.CommentRepository;
+		_unityOfWork = unitOfWork;
 	}
 
 
@@ -24,6 +28,32 @@ public class CommentController : ControllerBase
 		Comment? comment = await _commentRepository.GetById(id);
 		if (comment == null)
 			return NotFound();
+
+		return Ok(comment.ToResponse());
+	}
+
+	[HttpPost]
+	public async Task<ActionResult<CommentResponse?>> Post(PostNewCommentCommand command)
+	{
+		PostNewCommentHandler handler = new(_unityOfWork);
+
+		// execute
+		Comment? comment = await handler.ExecuteAsync(command);
+		if (comment == null)
+			return Conflict();
+
+		return Ok(comment.ToResponse());
+	}
+
+	[HttpPost("reply")]
+	public async Task<ActionResult<CommentResponse?>> PostReply(PostNewReplyCommentCommand command)
+	{
+		PostNewReplyCommentHandler handler = new(_unityOfWork);
+
+		// execute
+		Comment? comment = await handler.ExecuteAsync(command);
+		if (comment == null)
+			return Conflict();
 
 		return Ok(comment.ToResponse());
 	}
