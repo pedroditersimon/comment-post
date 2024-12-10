d436a74d3eaea8cb33ac728fdb61c66e35016d27
pedroditersimon <114523969+pedroditersimon@users.noreply.github.com>
2024-12-10 05:35:06 -0300

    user auth migration

--- CommentPost.Infrastructure/EntityFramework/Migrations/20241210083354_AddUserAuth.Designer.cs
+++ CommentPost.Infrastructure/EntityFramework/Migrations/20241210083354_AddUserAuth.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using CommentPost.Infrastructure.EntityFramework;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace CommentPost.Infrastructure.EntityFramework.Migrations
 {
     [DbContext(typeof(ApplicationDBContext))]
-    partial class ApplicationDBContextModelSnapshot : ModelSnapshot
+    [Migration("20241210083354_AddUserAuth")]
+    partial class AddUserAuth
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ CommentPost.Infrastructure/EntityFramework/Migrations/20241210083354_AddUserAuth.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CommentPost.Infrastructure.EntityFramework.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddUserAuth : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Name",
+                table: "User",
+                newName: "LastLoginAt");
+
+            migrationBuilder.AddColumn<string>(
+                name: "AuthProvider",
+                table: "User",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "DisplayName",
+                table: "User",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "ExternalId",
+                table: "User",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "PasswordHash",
+                table: "User",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ProfilePhotoUrl",
+                table: "User",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Username",
+                table: "User",
+                type: "text",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AuthProvider",
+                table: "User");
+
+            migrationBuilder.DropColumn(
+                name: "DisplayName",
+                table: "User");
+
+            migrationBuilder.DropColumn(
+                name: "ExternalId",
+                table: "User");
+
+            migrationBuilder.DropColumn(
+                name: "PasswordHash",
+                table: "User");
+
+            migrationBuilder.DropColumn(
+                name: "ProfilePhotoUrl",
+                table: "User");
+
+            migrationBuilder.DropColumn(
+                name: "Username",
+                table: "User");
+
+            migrationBuilder.RenameColumn(
+                name: "LastLoginAt",
+                table: "User",
+                newName: "Name");
+        }
+    }
+}
