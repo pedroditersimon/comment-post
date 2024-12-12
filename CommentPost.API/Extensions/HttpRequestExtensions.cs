32a1ca47830168a6bf3a4e4b9cadbf5660ee22b8
pedroditersimon <114523969+pedroditersimon@users.noreply.github.com>
2024-12-12 17:28:10 -0300

    auth token from http request

---
+++ CommentPost.API/Extensions/HttpRequestExtensions.cs
@@ -0,0 +1,19 @@
+﻿using CommentPost.Infrastructure.Models.Auth;
+
+namespace CommentPost.API.Extensions;
+
+public static class HttpRequestExtensions
+{
+
+	public static AuthToken? GetAuthToken(this HttpRequest request)
+	{
+		string authorization = request.Headers.Authorization.ToString();
+
+		if (string.IsNullOrWhiteSpace(authorization)
+			|| !authorization.StartsWith("Bearer ", StringComparison.OrdinalIgnoreCase))
+			return null;
+
+		string token = authorization.Substring("Bearer ".Length).Trim();
+		return new AuthToken() { Token = token };
+	}
+}
